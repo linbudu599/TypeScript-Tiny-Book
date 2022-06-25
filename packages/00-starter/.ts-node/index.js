19b4d1517f7962cb5866789c3fd585ef0fa3f023
linbudu <linbudu599@gmail.com>
2022-06-25 18:53:32 +0800

    feat: sync

---
+++ packages/00-starter/.ts-node/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tsd_1 = require("tsd");
+(0, tsd_1.expectType)('linbudu');
+console.log('Your TypeScript Playground works!');
+//# sourceMappingURL=index.js.map
