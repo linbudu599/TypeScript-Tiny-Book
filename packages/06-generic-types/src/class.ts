dd31d22618060e2e19798dd589a4552579bc3658
Linbudu <48507806+linbudu599@users.noreply.github.com>
2022-07-29 08:59:39 +0800

    Update class.ts

--- packages/06-generic-types/src/class.ts
+++ packages/06-generic-types/src/class.ts
@@ -22,3 +22,27 @@ class Queue<TElementType> {
     return this._list;
   }
 }
+class Queue<TElementType> {
+  private _list: TElementType[];
+
+  constructor(initial: TElementType[]) {
+    this._list = initial;
+  }
+
+  // 入队一个队列泛型子类型的元素
+  enqueue<TType extends TElementType>(ele: TType): TElementType[] {
+    this._list.push(ele);
+    return this._list;
+  }
+
+  // 入队一个任意类型元素（无需为队列泛型子类型）
+  enqueueWithUnknownType<TType>(element: TType): (TElementType | TType)[] {
+    return [...this._list, element];
+  }
+
+  // 出队
+  dequeue(): TElementType[] {
+    this._list.shift();
+    return this._list;
+  }
+}
